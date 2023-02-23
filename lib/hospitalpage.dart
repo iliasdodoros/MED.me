7fb7a02324b1b8ee80ab0951256c38c60b894cdf
ntua-el19206 <el19206@mail.ntua.gr>
2023-02-24 00:38:34 +0200

    hospitalpage still in development

--- lib/hospitalpage.dart
+++ lib/hospitalpage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class HospitalPage extends StatefulWidget {
@@ -32,31 +34,72 @@ class _HospitalPage extends State<HospitalPage> {
     setState(() {
       currentPageIndex = index;
     });
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => Hospitals(
+                currentPageIndex: currentPageIndex,
+              )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (currentPageIndex == 1) {
-      return ListView.separated(
-        padding: const EdgeInsets.all(10),
-        itemCount: clinics.length,
-        itemBuilder: (BuildContext context, int counter) {
-          return ListTile(
-            tileColor: Theme.of(context).canvasColor,
-            onTap: () => {_onTap(counter)},
-            title: Text(clinics[counter]),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) => const Divider(
-          thickness: 0,
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Theme.of(context).primaryColorLight),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).canvasColor,
+          title: Text(
+            "Clinics",
+            style: TextStyle(
+                fontSize: 28, color: Theme.of(context).primaryColorLight),
+          ),
         ),
-      );
-    } else {
-      return const Scaffold(
-        body: Center(
-          child: Text('Hospital Page'),
+        body: ListView.separated(
+          padding: const EdgeInsets.all(10),
+          itemCount: clinics.length,
+          itemBuilder: (BuildContext context, int counter) {
+            return ListTile(
+              tileColor: Theme.of(context).canvasColor,
+              onTap: () => {_onTap(counter)},
+              title: Text(clinics[counter]),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(
+            thickness: 0,
+          ),
+        ));
+  }
+}
+
+const jsondata = '{ "hospitals": [ "Γενικό Κρατικό","Στρατιωτικό Νοσοκομείο"]}';
+// const jsondata = '{ "hospitals": "Γενικό Κρατικό"}';
+
+class Hospitals extends StatelessWidget {
+  Hospitals({super.key, required this.currentPageIndex});
+
+  final hospitallist = jsonDecode(jsondata);
+  final int currentPageIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColorLight),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).canvasColor,
+        title: Text(
+          "Hospitals",
+          style: TextStyle(
+              fontSize: 28, color: Theme.of(context).primaryColorLight),
         ),
-      );
-    }
+      ),
+      body: Center(
+        child: Text('''${hospitallist['hospitals'][1]}'''),
+      ),
+    );
   }
 }
