fa9e9dcdb039b0ac82cca4d2fee0074bb5190445
ntua-el19155 <67788054+iliachilia@users.noreply.github.com>
2023-02-27 23:14:38 +0200

    changed app icon

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -3,24 +3,59 @@ import 'package:flutter/material.dart';
 import 'package:medme/diary.dart';
 import 'package:medme/medexams.dart';
 import 'package:medme/prescriptions.dart';
-
 import 'package:medme/infopage.dart';
+
 import 'package:table_calendar/table_calendar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePage();
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  File _imageFile = File('');
+
+  Future<void> _handleCircleTap() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      final imageFile = File(pickedFile.path);
+      // TODO: Store image file and update circle display
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
-
+        children: [
           const Expanded(
             child: SizedBox(
               height: 350,
               width: 100,
+            ),
+          ),
+          GestureDetector(
+            onTap: _handleCircleTap,
+            child: Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromARGB(255, 119, 234, 244),
+              ),
+              child: ClipOval(
+                child: _imageFile != null
+                    ? Image.file(
+                        _imageFile,
+                        fit: BoxFit.cover,
+                      )
+                    : Container(),
+              ),
             ),
           ),
           Expanded(
@@ -29,7 +64,6 @@ class ProfilePage extends StatelessWidget {
                 thickness: 10,
                 width: 10,
                 color: Theme.of(context).canvasColor,
-
               ),
               Expanded(
                 child: SizedBox(
@@ -47,7 +81,8 @@ class ProfilePage extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const Info()),
+                          MaterialPageRoute(
+                              builder: (context) => const InfoPage()),
                         );
                       },
                       child: const Text('INFO'),
@@ -158,99 +193,6 @@ class ProfilePage extends StatelessWidget {
             ),
           ]))
         ],
-      ),
-    );
-  }
-}
-
-
-
-
-class MedicalExams extends StatelessWidget {
-  const MedicalExams({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Exams'),
-      ),
-      body: Center(
-        child: Text('This is the Medical Exams page'),
-      ),
-    );
-  }
-}
-
-class Diary extends StatefulWidget {
-  const Diary({Key? key}) : super(key: key);
-
-  @override
-  State<Diary> createState() => _Diary();
-}
-
-class _Diary extends State<Diary> {
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Diary'),
-      ),
-      body: TableCalendar(
-        //calendarController: _calendarController,
-        firstDay: DateTime.utc(2013, 10, 16),
-        lastDay: DateTime.utc(2033, 3, 14),
-        focusedDay: _focusedDay,
-        selectedDayPredicate: (day) {
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay; // update _focusedDay here as well
-            });
-          }
-        },
-        startingDayOfWeek: StartingDayOfWeek.monday,
-        calendarFormat: _calendarFormat,
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          _focusedDay = focusedDay;
-        },
-        //To style the Calendar
-        calendarStyle: CalendarStyle(
-          isTodayHighlighted: true,
-          selectedDecoration: BoxDecoration(
-            color: Colors.red,
-            shape: BoxShape.circle,
-          ),
-          selectedTextStyle: TextStyle(color: Colors.white),
-          todayDecoration: BoxDecoration(
-            color: Color.fromARGB(255, 247, 158, 144),
-            shape: BoxShape.circle,
-          ),
-          defaultDecoration: BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-          weekendDecoration: BoxDecoration(
-            shape: BoxShape.circle,
-          ),
-        ),
-        headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-        ),
       ),
     );
   }
